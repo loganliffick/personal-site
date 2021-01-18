13200c343f2bf08cd74e00021eb4995c5f4433c5
Logan Liffick <loganliffick@mbp.local>
2021-01-18 16:27:35 -0500

    test 2

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title style="color: orange">Logan Liffick</title>
+        <title>Logan Liffick</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
