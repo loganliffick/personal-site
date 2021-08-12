a7affb9639c4bf96e86667300fc47f699988caa2
Logan Liffick <ltliffick@gmail.com>
2021-08-12 01:17:58 -0400

    v1 launch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
-import styled, { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme, GlobalStyle } from '../components/themes';
-import Button from '../components/button';
 
-// const useThemeDetector = () => {
-//   const getMatchMedia = () => window.matchMedia('(prefers-color-scheme: dark)');
-
-//   const [isDarkTheme, setIsDarkTheme] = useState(getMatchMedia().matches);
-
-//   const mqListener = (e) => {
-//     setIsDarkTheme(e.matches);
-//   };
-
-//   useEffect(() => {
-//     const mq = getMatchMedia();
-//     mq.addListener(mqListener);
-//     return () => mq.removeListener(mqListener);
-//   }, []);
-//   return isDarkTheme;
-// };
-
-const Main = styled.main`
-  align-items: center;
-  color: ${(props) => props.theme.primary};
-  display: flex;
-  height: 100vh;
-  justify-content: center;
-  width: 100vw;
-
-  & span {
-    color: ${(props) => props.theme.tertiary};
-  }
-
-  @media only screen and (max-width: 560px) {
-    display: block;
-    height: auto;
-  }
-`;
-
-const Section = styled.section`
-  margin: 0 64px;
-  width: 1044px;
-
-  @media only screen and (max-width: 560px) {
-    margin: 32px 24px 0px 24px;
-    width: auto;
-  }
-`;
+function Button(props) {
+  return (
+    <a href={props.url} target="_blank" rel="noopener">
+      {props.name}
+    </a>
+  );
+}
 
 export default function Home() {
-  //const isDarkTheme = useThemeDetector();
+  console.log(`
+
+▄▄▄▄▄▄▄░▄▄▄▄▄▄▄░▄▄▄▄▄▄░▄▄▄▄▄
+░░▀███░░░░▀██░░░░██▀░░░░██░░
+░░░▀██░░░░░▀██░░▄█░░░░░▄█░░░
+░░░░███░░░░░▀██▄█░░░░░░█░░░░
+░░░░░███░░░░░▀██░░░░░░█▀░░░░
+░░░░░░███░░░░▄███░░░░█▀░░░░░
+░░░░░░░██▄░░▄▀░███░░█▀░░░░░░
+░░░░░░░▀██▄█▀░░░███▄▀░░░░░░░
+░░░░░░░░▀██▀░░░░░███░░░░░░░░
+░░░░░░░░░▀▀░░░░░░░▀░░░░░░░░░ hat's up? 👀 
+
+
+`);
 
   return (
     <>
@@ -59,7 +33,7 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Logan Liffick is a designer and developer specializing in web and product with a background in illustration and animation."
+          content="Logan Liffick is an interactive designer and frontend developer. He builds brands, systems, and products."
         />
         <link rel="icon" href="/favicon.ico" />
         <link
@@ -70,34 +44,30 @@ export default function Home() {
           crossOrigin=""
         />
       </Head>
-      {/* <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}> */}
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <Main>
-          <Section>
-            <h1>
-              <span>©</span> 2021 Logan Liffick
-            </h1>
-            <h2>
-              Showing up M-F at{' '}
-              <Button url="https://digitalocean.com" name="DigitalOcean" />{' '}
-              Learn more about my work on{' '}
-              <Button url="https://read.cv/logan_liffick" name="read.cv" /> and{' '}
-              <Button url="https://github.com/logan-liffick" name="Github" />{' '}
-              Follow me on{' '}
-              <Button url="https://twitter.com/logan_liffick" name="Twitter" />{' '}
-              and{' '}
-              <Button
-                url="https://instagram.com/logan_liffick"
-                name="Instagram"
-              />{' '}
-              Send me an{' '}
-              <Button url="mailto:hello@loganliffick.com" name="email" /> to
-              connect.
-            </h2>
-          </Section>
-        </Main>
-      </ThemeProvider>
+      <main>
+        <section>
+          <h1>
+            <span>©</span> 2021 Logan Liffick
+          </h1>
+          <h2>
+            Showing up M-F at{' '}
+            <Button url="https://digitalocean.com" name="DigitalOcean" /> Learn
+            more about my work on{' '}
+            <Button url="https://read.cv/logan_liffick" name="read.cv" /> and{' '}
+            <Button url="https://github.com/logan-liffick" name="Github" />{' '}
+            Follow me on{' '}
+            <Button url="https://twitter.com/logan_liffick" name="Twitter" />{' '}
+            and{' '}
+            <Button
+              url="https://instagram.com/logan_liffick"
+              name="Instagram"
+            />{' '}
+            Send me an{' '}
+            <Button url="mailto:hello@loganliffick.com" name="email" /> to
+            connect.
+          </h2>
+        </section>
+      </main>
     </>
   );
 }
