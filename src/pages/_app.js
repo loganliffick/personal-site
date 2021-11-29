05fd9a5704bd4322b2dcc509b8287e5bec8b0afc
Logan Liffick <ltliffick@gmail.com>
2021-11-29 15:48:01 -0500

    analytics support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}; gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
