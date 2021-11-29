42a88173884ac23844a7a49083b135505f47d3d6
Logan Liffick <ltliffick@gmail.com>
2021-11-29 17:54:09 -0500

    seo launch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Script strategy="lazyOnload">
         {`window.dataLayer = window.dataLayer || []; function gtag()
           {dataLayer.push(arguments)}; gtag('js', new Date());
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
       </Script>
 
