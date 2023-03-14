ee03b3e837dc8e1c441e03d6cadf1e54ca07ee6a
Logan Liffick <ltliffick@gmail.com>
2023-03-13 23:32:02 -0400

    finished projects layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,16 @@ import moment from 'moment';
 
 const Footer = () => (
   <footer>
-    <p>{moment().format(' dddd, MMMM D')}</p>
-    <button
-      onClick={() => {
-        console.log('does nothing');
-      }}
-    >
-      Contact
-    </button>
+    <div>
+      <p>{moment().format(' dddd, MMMM D')}</p>
+      <button
+        onClick={() => {
+          console.log('does nothing');
+        }}
+      >
+        Contact
+      </button>
+    </div>
   </footer>
 );
 
