e1f444e064efc806762961951ed5c4ee5f10cbe7
Logan Liffick <ltliffick@gmail.com>
2023-12-23 16:29:30 -0500

    twitter test

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,39 @@
+import Button from 'components/Button';
+import { auth } from 'lib/firebase';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from 'lib/firebase';
 
 const Nav = () => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  console.log('is user ' + user);
+  console.log('is loading ' + loading);
+
   return (
     <header className="mt-8 flex w-full items-center justify-center px-6 text-zinc-800 sm:px-10">
-      <nav className="w-full max-w-5xl">
+      <nav className="flex w-full max-w-5xl justify-between">
         <Link className="text-xl font-medium" href="/">
           Logan Liffick
         </Link>
         {user ? (
-          <Image
-            alt={user.displayName || ''}
-            height={40}
-            src={user.photoURL || ''}
-            width={40}
-            referrerPolicy="no-referrer"
-            className="rounded-full"
-            loading="lazy"
-          />
+          <div className="flex gap-2">
+            <Image
+              alt={user.displayName || ''}
+              height={40}
+              src={user.photoURL || ''}
+              width={40}
+              referrerPolicy="no-referrer"
+              className="rounded-full"
+            />
+            <Button
+              text="Log out"
+              onClick={() => {
+                auth.signOut();
+              }}
+            />
+          </div>
         ) : (
-          <Link href="/auth/login">Login</Link>
+          <Button text="Login" onClick={() => {}} />
         )}
       </nav>
     </header>
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,16 +1,32 @@
 import Button from 'components/Button';
 import Layout from 'components/Layout';
 import Section from 'components/Section';
+import {
+  GoogleAuthProvider,
+  TwitterAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 import { auth } from 'lib/firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { GoogleLogo } from 'phosphor-react';
+import { GoogleLogo, TwitterLogo } from 'phosphor-react';
 
 const Page = () => {
   const googleProvider = new GoogleAuthProvider();
+  const twitterProvider = new TwitterAuthProvider();
   const googleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-      console.log(result.user);
+      // console.log(result.user);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const twitterLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, twitterProvider);
+      // console.log(result.user);
+      return result;
     } catch (error) {
       console.log(error);
     }
@@ -20,6 +36,13 @@ const Page = () => {
       <Section>
         <Button onClick={googleLogin} text="Log in with Google" type="primary">
           <GoogleLogo weight="bold" />
+        </Button>
+        <Button
+          onClick={twitterLogin}
+          text="Log in with Twitter"
+          type="primary"
+        >
+          <TwitterLogo weight="bold" />
         </Button>
       </Section>
     </Layout>
