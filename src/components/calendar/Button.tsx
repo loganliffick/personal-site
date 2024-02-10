dd7b2bedbc8be7c084499cbf3f539babaab6c05a
Logan Liffick <ltliffick@gmail.com>
2024-02-10 16:00:27 -0500

    updated calendar button

--- src/components/calendar/Button.tsx
+++ src/components/calendar/Button.tsx
@@ -1,16 +1,29 @@
 import Link from 'next/link'
+import { ArrowRight } from 'phosphor-react'
 
 const Button = (props: { data: any }) => {
   return (
     <li className="mx-2">
       <Link
-        className="flex w-full rounded-2xl bg-white/30 px-4 py-6 text-base font-bold tracking-wide text-zinc-100 transition-transform sm:hover:bg-white/50 sm:active:scale-90"
+        className="group flex w-full items-center justify-between rounded-2xl bg-white py-1 pl-6 pr-1 text-base font-bold text-zinc-800 transition-[letter-spacing,color,transform] duration-200 ease-bounce active:scale-x-95 active:tracking-normal sm:text-zinc-600 sm:hover:bg-white sm:hover:tracking-wider sm:hover:text-zinc-800"
         href={props.data.link.href || ''}
         aria-label={props.data.link.text}
         rel={props.data.link.external ? 'no-opener no-referrer' : ''}
         target={props.data.link.external ? '_blank' : '_self'}
       >
         {props.data.link.text}
+        <div className="relative flex size-10 items-center justify-center overflow-hidden text-inherit">
+          <ArrowRight
+            weight="bold"
+            size={20}
+            className="absolute -translate-x-8 transition-transform duration-300 ease-bounce group-active:-translate-x-8 sm:group-hover:translate-x-0"
+          />
+          <ArrowRight
+            weight="bold"
+            size={20}
+            className="transition-transform duration-300 ease-bounce group-active:translate-x-0 sm:group-hover:translate-x-8"
+          />
+        </div>
       </Link>
     </li>
   )
