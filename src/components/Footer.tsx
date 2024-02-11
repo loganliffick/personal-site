f5045a0ffe5ff932f39e2c3fb9532e9cd4437e51
Logan Liffick <ltliffick@gmail.com>
2024-02-10 20:17:05 -0500

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import moment from 'moment'
+import { useEffect, useState } from 'react'
 
-const Footer = () => (
-  <footer className="flex w-full items-center justify-center px-6 pb-8 pt-40 text-zinc-500 sm:px-10">
-    <div className="flex w-full max-w-lg items-center justify-center">
-      <p>{moment().format('dddd, MMMM D')}</p>
-    </div>
-  </footer>
-)
+const Footer = () => {
+  const [date, setDate] = useState('string')
+
+  useEffect(() => {
+    setDate(moment().format('dddd, MMMM D'))
+  }, [])
+
+  return (
+    <footer className="flex w-full items-center justify-center px-6 pb-8 pt-40 text-zinc-500 sm:px-10">
+      <div className="flex w-full max-w-lg items-center justify-center">
+        <p>{date}</p>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
