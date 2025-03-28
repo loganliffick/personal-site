30170fcc1e0cc113954245269425aef3caa6990f
loganliffick <ltliffick@gmail.com>
2025-03-27 23:58:02 -0400

    init dropdown

---
+++ src/components/ds/dropdown.tsx
@@ -0,0 +1,13 @@
+type DropdownProps = {
+  className?: string
+}
+
+const Dropdown = ({ className }: DropdownProps) => {
+  return (
+    <>
+      <div className="">Text</div>
+    </>
+  )
+}
+
+export default Dropdown
