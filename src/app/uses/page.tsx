6527696f9721847f6122de4f8f12913dad3e71e8
loganliffick <ltliffick@gmail.com>
2025-04-24 12:15:39 -0400

    updated badge

--- src/app/uses/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Badge } from '@/components/Badge'
-
-export default function Page() {
-  return (
-    <section className="flex min-h-screen w-full items-center justify-center">
-      <Badge />
-    </section>
-  )
-}
