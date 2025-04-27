8f43d637c90be0dcab0895d0c8781c4c576d3a70
loganliffick <ltliffick@gmail.com>
2025-04-26 22:34:16 -0400

    fix backbutton

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,7 +7,7 @@ export const BackButton = () => {
   return (
     <div className="sticky top-8 z-50 sm:h-0">
       <Link
-        className="reveal reveal-sm border-base-border bg-base-100 text-base-medium add-focus absolute -top-0.5 right-0 flex items-center justify-center gap-1 rounded-full border px-3 py-1 text-sm shadow-xs transition-transform md:-left-14 md:size-9 md:p-0 md:hover:scale-90"
+        className="reveal reveal-sm border-base-border bg-base-100 text-base-medium add-focus absolute -top-0.5 right-0 flex items-center justify-center gap-1 rounded-full border py-1 pr-3 pl-2.5 text-sm shadow-xs transition-transform md:-left-14 md:size-9 md:p-0 md:hover:scale-90"
         style={{ animationDelay: '150ms' }}
         href={'/'}
       >
