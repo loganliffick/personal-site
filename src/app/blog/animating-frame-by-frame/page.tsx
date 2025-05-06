73685d2dec06f41b555a546b2c4c4b99828a40fb
loganliffick <ltliffick@gmail.com>
2025-05-05 23:45:57 -0400

    cleanup article

--- src/app/blog/animating-frame-by-frame/page.tsx
+++ src/app/blog/animating-frame-by-frame/page.tsx
@@ -22,45 +22,7 @@ export default function Page() {
           />
           <Article>
             <h2>You in fact can just do things</h2>
-            <p>
-              I've been obsessed with creating the perfect sanctuary since the
-              world went remote. A space that works for how I work where I can
-              create without distraction from the outside world.
-            </p>
-            <p>
-              It started with a standing desk, crafted from a slab of butcher's
-              block and a Fully Jarvis frame, a sleek fusion of mechanical
-              precision and organic warmth. Serving as the studio's focal point,
-              the desk sets the tone for the entire space: light wood tones,
-              matte black accents, and inviting, cozy furnishings.
-            </p>
-            <p>
-              As with all hobbies, I began pouring more and more time into the
-              space rearranging furniture, experimenting with accent pieces, and
-              picking up new additions along the way. Most importantly, I
-              finally tackled some much needed cord management.
-            </p>
-            <p>
-              It turned out to be a worthy investment and was featured on{' '}
-              <TextLink
-                external
-                href="https://workspaces.xyz/"
-                title="Workspaces"
-              />{' '}
-              in its{' '}
-              <TextLink
-                external
-                href="https://www.workspaces.xyz/p/209-logan-liffick"
-                title="209th"
-              />{' '}
-              and{' '}
-              <TextLink
-                external
-                href="https://www.workspaces.xyz/p/292-logan-liffick"
-                title="292nd"
-              />{' '}
-              editions.
-            </p>
+            <p>A friendly reminder.</p>
           </Article>
         </Inset>
       </Section>
